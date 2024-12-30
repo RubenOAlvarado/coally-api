67b957739287c3f9481593ccffc5b6640df5a62a
Ruben Alvarado <ruben.alvardo@plexusworldwide.com>
2024-12-29 23:06:50 -0600

    fix: not launch validations when description is null or empty

--- src/infrastructure/http/validators/TaskValidator.ts
+++ src/infrastructure/http/validators/TaskValidator.ts
@@ -6,7 +6,7 @@ export const createTaskValidation = [
         .isLength({ min: 3, max: 255 }).withMessage('Title must be between 3 and 255 characters')
         .notEmpty().withMessage('Title is required'),
     check('description')
-        .optional()
+        .optional({ nullable: true, checkFalsy: true })
         .isString().withMessage('Description must be a string')
         .isLength({ min: 3, max: 255 }).withMessage('Description must be between 3 and 255 characters'),
 ];
@@ -20,17 +20,17 @@ export const updateTaskValidation =[
     param('id')
         .isMongoId().withMessage('Invalid task id'),
     check('title')
-        .optional()
+        .optional({ nullable: true, checkFalsy: true })
         .isString().withMessage('Title must be a string')
         .isLength({ min: 3, max: 255 }).withMessage('Title must be between 3 and 255 characters'),
     check('description')
-        .optional()
+        .optional({ nullable: true, checkFalsy: true })
         .isString().withMessage('Description must be a string')
         .isLength({ min: 3, max: 255 }).withMessage('Description must be between 3 and 255 characters'),
 ];
 
 export const getAllTasksValidation =[
     query('status')
-        .optional()
+        .optional({ nullable: true, checkFalsy: true })
         .isIn(['completed', 'pending']).withMessage('Status must be either "completed" or "pending"'),
 ];
